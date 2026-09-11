0174f10e4a5fc0c372b89215ce8c143b69bf17df
João Vitor Poliani <joaovitorpolianijvp@gmail.com>
2026-09-11 08:18:50 -0300

    atv-09-10

--- atividade-09-validador-senha/main.py
+++ atividade-09-validador-senha/main.py
@@ -1,1 +1,17 @@
-print("Olá, Mundo!")
+senha = input("Digite a senha: ")
+
+tamanhoValido = len(senha) >= 8
+possuiMaiuscula = False
+possuiNumero = False
+
+for char in senha:
+    if char.isupper():
+        possuiMaiuscula = True
+
+    if char.isdigit():
+        possuiNumero = True
+
+if tamanhoValido and possuiMaiuscula and possuiNumero:
+    print("Senha válida!")
+else:
+    print("Senha inválida!")
--- atividade-10-manipulacao-vetores/main.py
+++ atividade-10-manipulacao-vetores/main.py
@@ -1,1 +1,22 @@
-print("Olá, Mundo!")
+numeros = [None] * 10
+soma = 0
+
+for i in range(0, 10):
+    numeros[i] = int(input(f"Digite o {i + 1}º número: "))
+    soma += numeros[i]
+
+maior = numeros[0]
+menor = numeros[0]
+
+for numero in numeros:
+    if numero > maior:
+        maior = numero
+
+    if numero < menor:
+        menor = numero
+
+media = soma / len(numeros)
+
+print(f"Maior número: {maior}")
+print(f"Menor número: {menor}")
+print(f"Média dos números: {media}") 
