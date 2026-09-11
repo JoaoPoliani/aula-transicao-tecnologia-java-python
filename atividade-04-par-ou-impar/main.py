a45c89d123273e294658e10f31c574ab2bcfae2b
João Vitor Poliani <joaovitorpolianijvp@gmail.com>
2026-09-11 20:39:16 -0300

    Atividades que passaram
    Atividades que passaram sem querer

--- atividade-04-par-ou-impar/main.py
+++ atividade-04-par-ou-impar/main.py
@@ -1,1 +1,8 @@
-print("Olá, Mundo!")
+numero = int(input("Digite um numero inteiro: "))
+
+if numero == 0:
+    print("O numero é 0")
+elif numero % 2 == 0:
+    print("O numero é par")    
+else:
+    print("O numero é impar")   
--- atividade-05-maior-de-tres-numeros/main.py
+++ atividade-05-maior-de-tres-numeros/main.py
@@ -1,1 +1,13 @@
-print("Olá, Mundo!")
+num1 = int(input("Digite o primeiro numero inteiro: "))
+num2 = int(input("Digite o segundo numero inteiro: "))
+num3 = int(input("Digite o terceiro numero inteiro: "))
+
+maior = num1
+
+if num2 > maior:
+    maior = num2
+
+if num3 > maior:
+    maior = num3
+
+print(f"Maior numero: {maior}")
